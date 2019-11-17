e2b395e8ed7c24e97486a0ee79e5b65a0597e3b8
lGodHatesMel <56521233+lGodHatesMel@users.noreply.github.com>
2019-11-17 13:42:27 -0800

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,7 +1,9 @@
 config = {}
+
 config.rollingpaper = 1 -- Amount of papers needed
+
 config.weed = 1 -- Amount of 'weed' used
-config.maxjoints = 50 -- Max amount of joints a player can hold
+
 config.timetoroll = 10 * 1000 -- Time is in MS (0 * 1000 for 20 sec)
 
 config.addarmour = 7 -- Amount of armour per joint ?
